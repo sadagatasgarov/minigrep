97a3c79c4c6e584c2f6624c508427dbdcb805387
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-03-14 14:26:55 +0400

    Confige constriktor elace etdik

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::fs;
 fn main() {
     let args: Vec<String>= env::args().collect();
 
-    let l= parse_config(&args);
+    let config= Config::new(&args);
   
-    println!("Searching for {}", l.query);
-    println!("In file {}", l.file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(l.file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
     
     println!("With text:\n{contents}");
@@ -22,9 +22,12 @@ struct Config {
     file_path: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config{query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Config{query, file_path }
+    }
 }
+
