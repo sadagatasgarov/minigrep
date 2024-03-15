a8231337dc2af9b6e769f823af7c7de36a53fe61
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-03-15 17:00:50 +0400

    testden kecmis funksiyani run funksiyasina elave etdik

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@ use std::{error::Error, fs, result};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-
+    for line in search(&config.query, &contents){
+        println!("{line}")
+    };
     Ok(())
 }
 
