7b09d4b1e11f1d727ecb74de9c989bf75dd6707d
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-03-15 23:11:47 +0400

    Demek olar ki hazirdi errorlarin eprintln ile yazdirdiq

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs, result};
+use std::{env, error::Error, fs};
 
 
 
@@ -7,13 +7,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     for line in search(&config.query, &contents){
         println!("{line}")
     };
+
+    let results = if config.ignore_case{
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
 
 pub struct Config {
     pub query: String,
-    pub file_path: String
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -23,8 +35,9 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
     
-        Ok(Config{query, file_path })
+        Ok(Config{query, file_path, ignore_case })
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String>= env::args().collect();
 
     let config= Config::build(&args). unwrap_or_else(|err|{ 
-        println!("Arqumentleri Ayirma: {}", err);
+        eprintln!("Arqumentleri Ayirma: {}", err);
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
 
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
     
